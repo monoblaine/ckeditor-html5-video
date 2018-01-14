7d6adca21b8dd1e4740556a0a492dd15a73eb6b5
Serhan Apaydın <zerhan@gmail.com>
2018-01-14 15:58:26 +0300

    Add Turkish translations

---
+++ html5video/lang/tr.js
@@ -0,0 +1,17 @@
+CKEDITOR.plugins.setLang( 'html5video', 'en', {
+    button: 'Video ekle',
+    title: 'Video',
+    infoLabel: 'Video bilgisi',
+    allowed: 'Video bağlantısı (İzin verilen dosya uzantıları: MP4, WebM, Ogv)',
+    urlMissing: 'Video bağlantısı eksik.',
+    videoProperties: 'Video özellikleri',
+    upload: 'Gönder',
+    btnUpload: 'Sunucuya gönder',
+    advanced: 'Gelişmiş',
+    autoplay: 'Video otomatik olarak oynatılsın mı?',
+    yes: 'Evet',
+    no: 'Hayır',
+    loop: 'Video sürekli döngüde oynatılsın mı?',
+    responsive: 'Video genişliği ekran boyutuna göre ayarlansın',
+    controls: 'Oynatma düğmelerini göster'
+} );
